f6ceff2db1e804beba0906a30c9d3cbd1bd5c3bb
shiboying <boying.shi@appfolio.com>
2015-07-24 09:55:32 -0700

    bs : change favicon

--- askbot/conf/skin_general_settings.py
+++ askbot/conf/skin_general_settings.py
@@ -98,7 +98,7 @@ settings.register(
                         'at <a href="%(favicon_info_url)s">this page</a>.'
                     ) % {'favicon_info_url': const.DEPENDENCY_URLS['favicon']},
         allowed_file_extensions = ('ico',),#only allow .ico files
-        default = '/images/favicon.gif',
+        default = '/images/favicon.ico',
         url_resolver = skin_utils.get_media_url
     )
 )
