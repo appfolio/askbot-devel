6a6959ef2e3ffb7848f01101a21037bc8245a5dc
graham otte <graham.otte@appfolio.com>
2015-07-24 11:38:41 -0700

    go- why limit votes??

--- askbot/conf/vote_rules.py
+++ askbot/conf/vote_rules.py
@@ -20,7 +20,7 @@ settings.register(
     IntegerValue(
         VOTE_RULES,
         'MAX_VOTES_PER_USER_PER_DAY',
-        default=30,
+        default=10000,
         description=_('Number of votes a user can cast per day')
     )
 )
@@ -29,7 +29,7 @@ settings.register(
     IntegerValue(
         VOTE_RULES,
         'MAX_FLAGS_PER_USER_PER_DAY',
-        default=5,
+        default=10000,
         description=_('Maximum number of flags per user per day')
     )
 )
@@ -38,7 +38,7 @@ settings.register(
     IntegerValue(
         VOTE_RULES,
         'VOTES_LEFT_WARNING_THRESHOLD',
-        default=5,
+        default=1,
         description=_('Threshold for warning about remaining daily votes')
     )
 )
@@ -47,7 +47,7 @@ settings.register(
     IntegerValue(
         VOTE_RULES,
         'MAX_DAYS_TO_CANCEL_VOTE',
-        default=1,
+        default=-1,
         description=_('Number of days to allow canceling votes '
             '(-1 for no limit)')
     )
@@ -66,7 +66,7 @@ settings.register(
     IntegerValue(
         VOTE_RULES,
         'MIN_FLAGS_TO_HIDE_POST',
-        default=2,
+        default=10000,
         description=_('Number of flags required to automatically hide posts')
     )
 )
@@ -75,7 +75,7 @@ settings.register(
     IntegerValue(
         VOTE_RULES,
         'MIN_FLAGS_TO_DELETE_POST',
-        default=3,
+        default=10000,
         description=_('Number of flags required to automatically delete posts')
     )
 )
@@ -84,7 +84,7 @@ settings.register(
     IntegerValue(
         VOTE_RULES,
         'MIN_DAYS_FOR_STAFF_TO_ACCEPT_ANSWER',
-        default=7,
+        default=10000,
         description=_('Minimum days to accept an answer, '
             'if it has not been accepted by the question poster')
     )
