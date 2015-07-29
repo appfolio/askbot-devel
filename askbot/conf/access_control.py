f3a6be0946852bbf4b5a57fdc5c0225d7ab05cd7
Eric Hawkins <eric.hawkins@appfolio.com>
2015-07-29 11:52:13 -0700

    eah - allow other email domains

--- askbot/conf/access_control.py
+++ askbot/conf/access_control.py
@@ -72,7 +72,7 @@ settings.register(
     livesettings.LongStringValue(
         ACCESS_CONTROL,
         'ALLOWED_EMAIL_DOMAINS',
-        default='appfolio.com',
+        default='appfolio.com rentlinx.com mycase.com',
         description=_('Allowed email domain names'),
         help_text=_('Please use space to separate the entries, do not use the @ symbol!')
     )
